565be82813ae9bd9aefbbfaca01758ed92f473fb
yunan2016 <v-nany@hotmail.com>
2017-04-07 18:04:46 +0800

    Add files via upload

---
+++ About.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            bool IsAuthenticated = (System.Web.HttpContext.Current.User != null) && System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
+
+            if (!Request.IsAuthenticated)
+            {
+               // Response.Redirect("Login.aspx");
+            }
+        }
+    }
+}
